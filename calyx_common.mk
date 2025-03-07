848f53b9c10eec63df49c7d2cab0edd4afdc7dae
Michael Bestas <mkbestas@gmail.com>
2025-03-07 03:12:41 +0200

    zumapro: Include Pixel experiments
    Change-Id: I6de3de0362d9b2b0e22666b7da0b3fc85c4b4fac

--- calyx_common.mk
+++ calyx_common.mk
@@ -12,6 +12,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
+# Experiments
+include device/google/gs-common/performance/experiments/experiments.mk
+
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
 
