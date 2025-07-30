f1d6793e13aade8cb604e442310a538e81bbae73
Michael Bestas <mkbestas@gmail.com>
2025-07-30 17:36:21 +0300

    zumapro: Disable camera extensions
    On Android 16, Pixel 9 series switched from PixelCameraServices.apk to
    PixelCameraServicesConnectivityClient.apk which has broader permissions
    including internet access, so we had commented it out in the proprietary
    files lists. Having these props set without the apk causes 3rd party
    camera apps to freeze for ~10 seconds when opening the app or switching
    between cameras, so remove the camera extensions altogether.
    
    Change-Id: Icf490c1bcc85130e9c22f2b07fcbf3dc82ec580d

--- calyx_common.mk
+++ calyx_common.mk
@@ -7,11 +7,6 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-calyx
 
-# Camera
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
-    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
-
 # Lineage Health
 $(call soong_config_set,lineage_health,charging_control_supports_deadline,true)
 $(call soong_config_set,lineage_health,charging_control_supports_limit,true)
