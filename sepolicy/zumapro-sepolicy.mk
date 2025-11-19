slogar25/los_device_google_zumapro

zumapro: Update sepolicy path
Change-Id: Ie2d420cd25e1a8b65977504ed987df57b525dc5f

sepolicy/zumapro-sepolicy.mk
@@ -1,25 +1,25 @@
 # sepolicy that are shared among devices using zumapro
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/vendor
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/radio
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/radio/private
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/radio
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/radio/private
 
 # unresolved SELinux error log with bug tracking
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/tracking_denials
 
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro-sepolicy/public
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/private
 
 # system_ext
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro-sepolicy/system_ext/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/system_ext/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro/sepolicy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/system_ext/private
 
 # PowerStats HAL
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 
 # To be reviewed and removed.
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/whitechapel_pro
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/zuma/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/system_ext/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/system_ext/private
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/whitechapel_pro
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/zuma/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/system_ext/private
 

CalyxOS/device_google_zumapro

sepolicy/zumapro-sepolicy.mk
@@ -7,28 +7,28 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
 
 # sepolicy that are shared among devices using zumapro
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/aam
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/vendor
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/zumapro-sepolicy/radio
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/radio/private
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/aam
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/zumapro/sepolicy/radio
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/radio/private
 
 # unresolved SELinux error log with bug tracking
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/tracking_denials
 
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro-sepolicy/public
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/private
 
 # system_ext
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro-sepolicy/system_ext/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/system_ext/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro/sepolicy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/system_ext/private
 
 # PowerStats HAL
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 
 # To be reviewed and removed.
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/whitechapel_pro
-BOARD_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/zuma/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/system_ext/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro-sepolicy/legacy/system_ext/private
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/whitechapel_pro
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/zuma/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/legacy/system_ext/private
 